9bf0f3f237ca2cb600028b27c692a23bc008f800
zqz <z@qz.com>
2025-01-12 15:01:24 +0800

    update for search in selected folder

--- lua/options.lua
+++ lua/options.lua
@@ -22,3 +22,24 @@ require("nvim-tree").setup({
     ignore = false,
   },
 })
+
+-- Search in selected folder
+local function search_in_selected_folder()
+  local api = require("nvim-tree.api")
+  local node = api.tree.get_node_under_cursor()
+  -- warning: relative_path func is different from absolute_path, can't be used here.
+  local absolute_path = node.absolute_path
+  local tree_root = vim.fn.getcwd()
+  local relative_path = vim.fn.fnamemodify(absolute_path, ":." .. tree_root)
+  local folder_path = relative_path
+  if node.type == "file" then
+    folder_path = vim.fn.fnamemodify(relative_path, ":h")
+  end
+  require("telescope.builtin").live_grep({
+    cwd = folder_path,
+    prompt_title = "Search in: " .. folder_path,
+  })
+end
+
+
+vim.keymap.set("n", "<leader>fq", search_in_selected_folder, { desc = "grep in current dictionary." })
